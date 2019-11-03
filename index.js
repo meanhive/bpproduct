82a0b0ad8cabf56e1d7d9e958fb020a571a6333b
MEAN Hive <meanhive@zoho.com>
2019-11-03 12:19:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT = 5000;
+const PORT = Number(process.env.PORT || 5000);;
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
